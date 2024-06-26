0ec7cc7febb52c4d4d1d88a2f86d57ba6016347d
Mylawyer555 <samuelabel636@gmail.com>
2024-06-26 14:40:39 +0100

    TODO UPDATE

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -31,44 +31,47 @@ const Todo = () => {
 
   
   return (
-    <div className="Todo-wrapper"  >
-      <div className="up">
-       <h1>To-Do-list</h1>
-        
-      </div>
-      <div className="down">
-       {showInput &&  <div className="input-txt">
-          <input
-            type="text"
-            placeholder='Enter a task..'
-            value={newTask}
-            onChange={(handleInputChange)}
-          />
-
-          <button className='addButton' onClick={addTask}>ADD</button>
-        </div> }
-        
-       
-        <div className="tasks">
-          <h4>Today's Task</h4>
-          {tasks.map((task, index) =>
-            
-            <div className="loop-task" key={index}>
-              <p className='task-txt'>
-                {task}
-              </p>
-
-              <button className='delete-btn' onClick={() => deleteTask(index)}>delete</button>             
-            </div>
+    <>
+      <div className="Todo-wrapper"  >
+        <div className="up">
+        <h1>To-Do-list</h1>
           
-          )}
         </div>
+        <div className="down">
+        {showInput &&  <div className="input-txt">
+            <input
+              type="text"
+              placeholder='Enter a task..'
+              value={newTask}
+              onChange={(handleInputChange)}
+            />
 
-        <div className="plus" onClick={() => setShowInput(true)}>
-          <h4>+</h4>
-       </div>
+            <button className='addButton' onClick={addTask}>ADD</button>
+          </div> }
+          
+        
+          <div className="tasks">
+            <h4>Today's Task</h4>
+            {tasks.map((task, index) =>
+              
+              <div className="loop-task" key={index}>
+                <p className='task-txt'>
+                  {task}
+                </p>
+
+                <button className='delete-btn' onClick={() => deleteTask(index)}>delete</button>             
+              </div>
+            
+            )}
+          </div>
+
+          <div className="plus" onClick={() => setShowInput(true)}>
+            <h4>+</h4>
+        </div>
+        </div>
       </div>
-    </div>
+        
+    </>
   );
 }
 
